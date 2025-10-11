d54a1e55887a3811c0c2aa4071214c53aa6c88c6
EvaristoGZ <evgarciaz@gmail.com>
2025-10-12 01:49:50 +0200

    fix: change snyk actions version

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ app = FastAPI()
 
 
 @app.get("/healthz")
-def healthz() -> dict[str, str]:
+def healthz():
     return {"status": "ok"}
 
 
 @app.get("/")
-def root() -> dict[str, str]:
+def root():
     return {"message": "Hello from CI/CD!"}
